e2779e8e0642dab6bdf0c4ba915caa1f6122fbc2
k-minyoung <alsdudsk12@naver.com>
2023-09-08 17:36:18 +0900

    비밀번호 찾기 구현

--- controller/CprofileEdit.js
+++ controller/CprofileEdit.js
@@ -5,6 +5,7 @@ const Cauth = require('./Cauth');
 const bcrypt = require('bcrypt');
 const nodemailer = require('nodemailer');
 const mailer = require('../routes/mail');
+const crypto = require('crypto')
 
 ///////////GET////////////
 const profileUpdate = async (req, res) => {
@@ -64,13 +65,15 @@ const profileDelete = async (req,res) => {
     }
 };
 
-
+const findPw = (req,res) => {
+    res.render('findpw')
+}
 ////////////POST//////////////
 const profileUpdatePost = async (req,res)=>{
     try {
-        const {id,nick} = req.body
+        const {id,nick,email} = req.body
 
-        const update = await User.update({nickname : nick}, {where : {user_id : id}})
+        const update = await User.update({nickname : nick, email,}, {where : {user_id : id}})
         res.json({result : true})
 
     } catch (error) {
@@ -90,11 +93,11 @@ const pwUpdatePost = async (req,res)=>{
             const update = await User.update({login_pw : hashPwEdit}, {where : {user_id : id}})
             res.json({result : true})
         } else if(!pwCompare && pw_edit === pw_edit2) {
-            res.send({message : '비밀번호가 틀립니다'})
+            res.json({result : false, message : '비밀번호가 틀립니다'})
         } else if(pwCompare && pw_edit !== pw_edit2) {
-            res.send({message : '비밀번호가 서로 다릅니다'})
+            res.json({result : false, message : '비밀번호가 서로 다릅니다'})
         } else {
-            res.send({message : '작성한 내용을 확인해주세요'})
+            res.json({result : false, message : '작성한 내용을 확인해주세요'})
         }
     } catch (error) {
         console.log(error)
@@ -111,24 +114,61 @@ const profileDeletePost = async (req,res) => {
             const destroy = await User.destroy({where : {user_id : id}})
             res.json({result : true})
         } else if(!pwCompare && login_id === Cid) {
-            res.send({message : '비밀번호가 틀립니다.'})
+            res.send({result : false, message : '비밀번호가 틀립니다.'})
         } else {
-            res.send({message : '존재하지 않는 회원입니다.'})
+            res.send({result : false, message : '존재하지 않는 회원입니다.'})
         }
     } catch (error) {
         console.log(error)
     }
 }
 
-const findPwPost = (req, res) => {
+//본인인지 확인 후 임시 해쉬 비밀번호를 DB에 저장, 가입시 입력한 email로 임시 비번 발송
+const findInfoPost = async (req,res) =>{ 
+    try {
+        const {login_id, name} = req.body
+        const user = await User.findOne({attributes: ["email","user_name"] , where:{login_id} , raw:true})
+        if(!user){
+            res.send({result : false,  message : '존재하지 않는 회원입니다.'})
+        }
+        if(user.user_name === name){
+            const email = user.email
+
+            const password = crypto.randomBytes(8).toString('base64')
+            const hash = await Cauth.pwHashing(password)
+
+            const update = await User.update({login_pw : hash},{where : {login_id}}) 
+            
+            let emailParam = {
+                toEmail: email,     // 수신할 이메일
+            
+                subject: `${login_id} 님 비밀번호 찾기 안내`,   // 메일 제목
+            
+                text: `${name} 님의 임시 비밀번호
+${password}
+로그인 시 비밀번호를 변경해 주세요. `  // 메일 내용, 들여쓰기도 공백으로 치기 때문에 옆으로 밀어놨어요
+              };
+            
+              mailer.sendGmail(emailParam);
+              
+              res.send({result : true, message : '계정에 입력한 email로 임시 비밀번호 발급 완료'})
+        }else {
+            res.send({result : false,  message : '아이디와 이름을 확인해주세요'})
+        }  
+    } catch (error) {
+        console.log(error)
+    }
+}
+
+const findPwPost = (req, res) => { // /mail 에서 받는 포스트
     const { email }  = req.body;
   
     let emailParam = {
       toEmail: email,     // 수신할 이메일
   
-      subject: 'New Email From Gyunny',   // 메일 제목
+      subject: '회원님 비밀번호 찾기 안내',   // 메일 제목
   
-      text: `Gyunny 회원님!`  // 메일 내용
+      text: `메일테스트`  // 메일 내용
     };
   
     mailer.sendGmail(emailParam);
@@ -141,9 +181,11 @@ module.exports ={
     profileUpdate,
     pwUpdate,
     profileDelete,
+    findPw,
     profileUpdatePost,
     pwUpdatePost,
     profileDeletePost,
+    findInfoPost,
     findPwPost,
 }
 
