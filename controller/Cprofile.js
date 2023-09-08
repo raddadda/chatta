a14ee063cebb0f3a08639dc4d46275f89364ac40
eukkki210 <ysg980423@gmail.com>
2023-09-08 17:41:21 +0900

    프로필 코드 수정

--- controller/Cprofile.js
+++ controller/Cprofile.js
@@ -2,22 +2,66 @@ const { User, Friend_List, Chat_Room_Join, Chat_Room, Chat_Message, Board, Board
 const constant = require('../common/constant');
 const Cauth = require('./Cauth');
 
-function calculateAge(birthdate) {
-    // 생년월일로 나이 계산
-    const birthDate = new Date(birthdate);
-    const currentDate = new Date();
+const profile = async (req, res) => {
+    try {
+        const getCheck = await Cauth.getAuthCheck(req, res);
+        if (!getCheck) {
+            res.redirect('/')
+            return;
+        }
+        const cookieValue = req.signedCookies.logined.id;
+        const userId = await Cauth.stringToUuid(cookieValue);
 
-    const age = currentDate.getFullYear() - birthDate.getFullYear();
+        const user = await User.findOne({ where: { user_id: userId } });
 
-    const currentMonth = currentDate.getMonth();
-    const birthMonth = birthDate.getMonth();
+        const friendCount = await getFriendCount(userId);
 
-    if (currentMonth < birthMonth || (currentMonth === birthMonth && currentDate.getDate() < birthDate.getDate())) {
-        return age - 1;
-    } else {
-        return age;
+        const ownerChatRooms = await getOwnerChatRooms(userId);
+        const userChatRooms = await getUserChatRooms(userId);
+        const allChatRooms = ownerChatRooms.concat(userChatRooms);
+
+        for (const room of allChatRooms) {
+            room.unreadMessages = await calculateUnreadMessages(room.id, userId);
+            room.latestUnreadMessage = await getLatestUnreadMessage(room.id, userId);
+        };
+
+        const bookmarkedBoards = await getBookmarkedBoards(userId);
+        const schedules = await getSchedules(userId);
+        const age = await calculateAge(user.birth);
+
+        if (!user) {
+            return res.status(404).render('404');
+        }
+
+        console.log(user, age, friendCount, ownerChatRooms, userChatRooms, bookmarkedBoards, schedules)
+        res.render('profile', { user, age, friendCount, ownerChatRooms, userChatRooms, bookmarkedBoards, schedules });
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({ message: '내부 서버 오류' });
     }
-}
+};
+
+const calculateAge = (birthdate) => {
+    // 생년월일로 나이 계산
+    try {
+        const birthDate = new Date(birthdate);
+        const currentDate = new Date();
+
+        const age = currentDate.getFullYear() - birthDate.getFullYear();
+
+        const currentMonth = currentDate.getMonth();
+        const birthMonth = birthDate.getMonth();
+
+        if (currentMonth < birthMonth || (currentMonth === birthMonth && currentDate.getDate() < birthDate.getDate())) {
+            return age - 1;
+        } else {
+            return age;
+        }
+    } catch (error) {
+        console.error('나이 계산 오류:', error);
+        throw error;
+    }
+};
 
 const getFriendCount = async (userId) => {
     try {
@@ -120,39 +164,6 @@ const getSchedules = async (userId) => {
     } catch (error) {
         console.error('일정 가져오기 오류:', error);
         throw error;
-    }
-};
-
-const profile = async (req, res) => {
-    try {
-        const cookieValue = req.signedCookies.logined.id;
-        const userId = await Cauth.stringToUuid(cookieValue);
-
-        const user = await User.findOne({ where: { user_id: userId } });
-
-        const friendCount = await getFriendCount(userId);
-
-        const ownerChatRooms = await getOwnerChatRooms(userId);
-        const userChatRooms = await getUserChatRooms(userId);
-        const allChatRooms = ownerChatRooms.concat(userChatRooms);
-
-        for (const room of allChatRooms) {
-            room.unreadMessages = await calculateUnreadMessages(room.id, userId);
-            room.latestUnreadMessage = await getLatestUnreadMessage(room.id, userId);
-        };
-
-        const bookmarkedBoards = await getBookmarkedBoards(userId);
-        const schedules = await getSchedules(userId);
-        const age = await calculateAge(user.birth);
-
-        if (!user) {
-            return res.status(404).render('404');
-        }
-
-        res.render('profile', { user, age, friendCount, ownerChatRooms, userChatRooms, bookmarkedBoards, schedules });
-    } catch (error) {
-        console.error(error);
-        res.status(500).json({ message: '내부 서버 오류' });
     }
 };
 
