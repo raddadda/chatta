807adb1cd5ab402f004bd917ccc088c5b503bc2d
raddadda <cyc123m@gmail.com>
2023-09-14 20:20:43 +0900

    last2

--- controller/Cprofile.js
+++ controller/Cprofile.js
@@ -35,8 +35,8 @@ const profile = async (req, res) => {
             return res.status(404).render('404');
         }
 
-       // const profileImage = await Cimage.getProfileImage(userId);
-       const profileImage = '';
+        const profileImage = await Cimage.getProfileImage(userId);
+
         res.render('profile', { user, age, profileImage, friendCount, posterChatRooms, userChatRooms, bookmarkedBoards, schedules });
         return;
     } catch (error) {
