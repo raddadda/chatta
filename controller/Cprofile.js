81ee50cc4735033c97135eea81eba4966b64de19
raddadda <cyc123m@gmail.com>
2023-09-15 02:02:51 +0900

    lastgit add .git add .

--- controller/Cprofile.js
+++ controller/Cprofile.js
@@ -28,8 +28,8 @@ const profile = async (req, res) => {
         if (!user) {
             return res.status(404).render('404');
         }
-        // const profileImage = await Cimage.getProfileImage(userId);
-        const profileImage = '';
+        const profileImage = await Cimage.getProfileImage(userId);
+        
         res.render('profile', { user, age, profileImage, friendCount, posterChatRooms, userChatRooms, bookmarkedBoards, schedules });
         return;
     } catch (error) {
