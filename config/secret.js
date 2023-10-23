19c3f1135b8c391bd102d94d353054e28f6a6716
raddadda <cyc123m@gmail.com>
2023-10-23 21:58:38 +0900

    test

---
+++ config/secret.js
@@ -0,0 +1,9 @@
+/////////////////////////cookie//////////////////////////
+const cookieSecret = '2e63b1f6-3820-4071-8bf0-3b0626edaa34'
+
+const salt ='asdfaasdf'
+
+module.exports = {
+    cookieSecret,
+    salt
+}
